a98f46daf1d30d2270ef30c2b3cde717597762ba
mikematar1 <mike.matar97@gmail.com>
2022-11-21 14:38:44 +0200

    update

--- api/login.php
+++ api/login.php
@@ -1,16 +1,18 @@
 <?php
+session_start();
 include("connection.php");
 $username = $_POST["username"];
 $password = $_POST["password"];
-$query = $conn->prepare("SELECT id,username,password FROM users WHERE username=? AND password=?");
+$query = $conn->prepare("SELECT * FROM users WHERE username=? AND password=?");
 $query->bind_param("ss",$username,$password);
 $query->execute();
 $result = $query->get_result();
 $user = $result->fetch_array();
 $response=[];
-if(!user){
+if(!$user){
     $response["found"]=false;
 }else{
-    $response["found"]=$user;   
+    $response["found"]=$user;
+    $_SESSION["userid"] = $user[2];
 }
 echo json_encode($response);
--- website/scripts/index.js
+++ website/scripts/index.js
@@ -8,12 +8,17 @@ function authenticate(){
     const xhr = new XMLHttpRequest();
 
     xhr.onload = function () {
-        // let response = JSON.parse(this.responseText)["found"];
-        console.log(this.responseText);
+        let response = JSON.parse(this.responseText)["found"];
+        if(!response){
+            //user was not found
+        }else{
+            //user was found and transition to home page
+            window.location.href="./home.html";
+        }
         
     }
 
     xhr.open("POST", "../api/login.php")
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(`name=${username}&pwd=${password}`);
+    xhr.send(`username=${username.value}&password=${password.value}`);
 }
